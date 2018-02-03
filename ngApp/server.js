459aebd001fc8987bfdea70cc229bd5065b85fc1
Vishwas Gopinath - I16165 <vishwas.gopinath@microchip.com>
2018-02-03 12:32:07 -0700

    Add server.js

---
+++ ngApp/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// const api = require('./server/routes/api');
+const port = 3000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(bodyParser.json()); 
+
+// app.use('/api', api);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(port, function(){
+    console.log("Server running on localhost:" + port);
+});
